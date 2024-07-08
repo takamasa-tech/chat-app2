6b97b1ca8c9c428bdbaacc5e16fbb99795ab0c7b
takamasa-tech <g1012075@icloud.com>
2024-07-08 12:12:38 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_08_022125) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_08_024841) do
   create_table "rooms", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
